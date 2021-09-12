5d36ad519c1f3088dc79858f48d8bcef0d4e40a1
Darja Dabetic <da.dabetic@levi9.com>
2021-09-13 01:24:21 +0200

    added finish page

--- game/src/components/Finish/Finish.js
+++ game/src/components/Finish/Finish.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-// import styles from './Finish.module.css';
+import {GameContext} from '../../contexts/GameContext';
 
-const Finish = () => <p>the end</p>;
+import styles from './Finish.module.css';
+
+class Finish extends React.Component {
+  render() {
+    return (
+      <GameContext.Consumer>
+        {(context) => {
+          const {score} = context;
+          const win = score.left ? false : score.hit > score.miss;
+          return (
+            <div className={styles.container}>
+              {win ? (
+                <p style={{color: 'green'}}>Congratulation</p>
+              ) : (
+                <p style={{color: 'red'}}>Wish you more luck next time!</p>
+              )}
+            </div>
+          );
+        }}
+      </GameContext.Consumer>
+    );
+  }
+}
 
 export default Finish;
